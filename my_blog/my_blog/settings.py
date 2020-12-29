855fcfcbe8273faae6057523a89c8274cc5ddffe
ans20xx <2781988166@qq.com>
2020-12-29 21:37:27 +0800

    12-29

--- my_blog/my_blog/settings.py
+++ my_blog/my_blog/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '&ba+q=t5vusliz1&53rdl^j46u!@qe8*^u++-$2ymiu4sg*__%'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["127.0.0.1","localhost","47.100.93.220",'.ancode.club']
 
